0aa55508cccdcd1fc7bd9a0aa7ac203c97ffa90b
Darryl L. Pierce <dpierce@redhat.com>
2012-04-18 11:38:58 -0400

    Updated the Ruby extensions to selectively check for dependencies.
    For either Ruby 1.8 or 1.9 it looks for the standard header files
    provided by the QPID development package.
    
    If the code is compiled under Ruby 1.8 then it checks for the
    qpidnonblockio shared library and the related header files.
    
    Merge with the Rakefile changes

--- qpid/cpp/bindings/qpid/ruby/ext/nonblockio/extconf.rb
+++ qpid/cpp/bindings/qpid/ruby/ext/nonblockio/extconf.rb
@@ -27,8 +27,53 @@ require 'mkmf'
 
 # specify the version of Ruby being used as a macro: RUBY##
 runtime_version = RbConfig::CONFIG["ruby_version"].gsub(/\./, "")[0, 2]
+using_ruby_18 = (runtime_version == "18")
+$CPPFLAGS << " -DRUBY#{runtime_version}"
 
-$CPPFLAGS += " -DRUBY#{runtime_version}"
+# tell Ruby to use a C++ compiler and to stop after preprocessing
+RbConfig::CONFIG['CPP'] = "g++ -E"
+
+dir_config('qpid')
+dir_config('nonblockio') if using_ruby_18
+
+def require_library(library)
+  fail("Missing library: #{library}") unless have_library(library)
+end
+
+def require_header(header)
+  fail("Missing header: #{header}") unless have_header(header)
+end
+
+# ensure the required libraries are present
+require_library("qpidclient")
+require_library("qpidcommon")
+require_library("qpidmessaging")
+require_library("qpidtypes")
+
+if using_ruby_18
+  require_library("qpidnonblockio")
+end
+
+# ensure the required headers are present
+require_header("qpid/messaging/exceptions.h")
+require_header("qpid/messaging/Address.h")
+require_header("qpid/messaging/Connection.h")
+require_header("qpid/messaging/Session.h")
+require_header("qpid/messaging/Receiver.h")
+require_header("qpid/messaging/Sender.h")
+require_header("qpid/messaging/Message.h")
+require_header("qpid/messaging/Duration.h")
+require_header("qpid/messaging/FailoverUpdates.h")
+
+#headers required by for Ruby 1.8
+if using_ruby_18
+#  require_header("qpid/messaging/synchio/Acknowledge.h")
+#  require_header("qpid/messaging/synchio/NextReceiver.h")
+#  require_header("qpid/messaging/synchio/ReceiverFetch.h")
+#  require_header("qpid/messaging/synchio/ReceiverGet.h")
+#  require_header("qpid/messaging/synchio/Send.h")
+#  require_header("qpid/messaging/synchio/SessionSync.h")
+end
 
 create_makefile('nonblockio')
 
